67321b383142e97daf6a73f90c14719dc7687b3d
Bruno <brunoymo@live,com>
2025-06-02 10:59:11 -0300

    run 5

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 """
 Script para iniciar o servidor da API.
 """
-import uvicorn
+from src.main import app
 
-if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8000)
+# Exponha a aplicação FastAPI para o Vercel
+app = app
