9df3d70966e1ff8daa9ee33691b1e167dd018e2f
Bruno <brunoymo@live,com>
2025-06-02 10:43:00 -0300

    run 3

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ Script para iniciar o servidor da API.
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("src.main:app", host="localhost", port=8000, reload=True)
